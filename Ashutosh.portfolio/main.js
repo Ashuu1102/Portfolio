f8e35c301152c59e653d06aaffe4307d6b5f35eb
Ashuu1102 <136330066+Ashuu1102@users.noreply.github.com>
2023-10-23 09:24:02 +0530

    Update main.js

--- Ashutosh.portfolio/main.js
+++ Ashutosh.portfolio/main.js
@@ -4,15 +4,3 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
-document.addEventListener("DOMContentLoaded", function() {
-    // Select the slidingText element
-    const slidingText = document.getElementById("title");
-
-    // Trigger the animation by adding a CSS class
-    slidingText.classList.add("slide-up");
-
-    // You can also remove the class to reset the animation if needed
-    setTimeout(() => {
-        slidingText.classList.remove("slide-up");
-    }, 5000); // Adjust the duration as needed
-});
